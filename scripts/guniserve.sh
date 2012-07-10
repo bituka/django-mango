120e7dd2183c4c366f98c9462203e0a61d584003
Marconi Moreto <caketoad@gmail.com>
2012-07-10 22:55:26 +0800

    added configurations and script for deployment

---
+++ scripts/guniserve.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+LOGFILE=/home/mango/logs/gunicorn.log
+LOGDIR=$(dirname $LOGFILE)
+NUM_WORKERS=3
+# user/group to run as
+USER=mango
+GROUP=pypug
+ADDRESS=unix:/tmp/gunicorn.sock
+cd /home/mango/django-mango/mango
+source /home/mango/mangoenv/bin/activate
+test -d $LOGDIR || mkdir -p $LOGDIR
+exec gunicorn mango.wsgi:application -w $NUM_WORKERS --bind=$ADDRESS \
+  --user=$USER --group=$GROUP --log-level=debug \
+  --log-file=$LOGFILE 2>>$LOGFILE
